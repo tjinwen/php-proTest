528dc440a85c309a87253c99fd67f91d19ee4210
tjinwen <tjinwen94128@gmail.com>
2016-04-16 20:23:56 +0800

    learn01 v1.0

--- test01/learn01.php
+++ test01/learn01.php
@@ -1,5 +1,16 @@
 <?php
 echo "<h2>PHP is Fun!</h2>";
 echo "Hello world!<br />";
-echo "this","string","was","made";
+echo "This ","string ","was ","made";
+
+echo "<br />#################define<br />";
+define("GREETING", "Welcome to Taiwan!",true);
+echo greeting;
+
+echo "<hr /><br />";
+$colors = array("red","green","blue");
+foreach ($colors as $value){
+    echo "$value <br />";
+}
+
 ?>
